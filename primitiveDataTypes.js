5c63b9654a132114e6e0b749a63d44533c2f8cd3
caathyn <caathynguyen@gmail.com>
2022-04-05 00:08:52 -0700

    add primitivedatatypes

---
+++ primitiveDataTypes.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// console.log('My name is ' + name);
+// console.log('I am ' + age + ' years old');
+// console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
+// console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
