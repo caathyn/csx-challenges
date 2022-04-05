083a98bdba292b92d05974a083863026c29f0cbb
caathyn <caathynguyen@gmail.com>
2022-04-05 00:28:16 -0700

    solution for primitivedatatypes

--- primitiveDataTypes.js
+++ primitiveDataTypes.js
@@ -1,7 +1,13 @@
 // ADD CODE HERE
+const name = 'Ashley Lee';
+const age = 33;
+const loveJavaScript = true;
+const listOfComplaints = null;
 
 // Uncomment these to check your work!
-// console.log('My name is ' + name);
-// console.log('I am ' + age + ' years old');
-// console.log('It\'s ' + loveJavaScript + ', I love JavaScript');
-// console.log('The list of things I hate about JavaScript is ' + listOfComplaints);
+console.log('My name is ' + name);
+console.log('I am ' + age + ' years old');
+console.log("It's " + loveJavaScript + ', I love JavaScript');
+console.log(
+  'The list of things I hate about JavaScript is ' + listOfComplaints
+);
