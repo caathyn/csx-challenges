1f933d9511cee7910efd10e8869f99e6f5c039a3
caathyn <caathynguyen@gmail.com>
2022-04-05 13:23:32 -0700

    solution string methods

---
+++ stringMethods.js
@@ -0,0 +1,13 @@
+let hometown = "New York City";
+let favAlbum = "The Rise and Fall of Ziggy Stardust and the Spiders From Mars"
+
+// ADD CODE BELOW
+const count = (hometown.length)
+const upper = (favAlbum.toUpperCase())
+const lower = (favAlbum.toLowerCase())
+
+
+// Uncomment the line below to check your work!
+console.log(count);
+console.log(upper);
+console.log(lower);
