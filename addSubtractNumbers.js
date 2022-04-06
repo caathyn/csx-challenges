50482d2018ff64819168d3d545536b06a47e5d08
caathyn <caathynguyen@gmail.com>
2022-04-05 22:28:57 -0700

    solved addsubtractnumbers

---
+++ addSubtractNumbers.js
@@ -0,0 +1,9 @@
+// ADD CODE BELOW
+const birthYear = 1980
+const age = 42
+const in10Yrs = age + 10
+
+// Uncomment these to check your work!
+console.log(birthYear);
+console.log(age);
+console.log(in10Yrs);
