6e2971a3cbdfc3901ca5e305d5748b879685985b
caathyn <caathynguyen@gmail.com>
2022-04-05 22:41:16 -0700

    solved typeofoperator

---
+++ typeofOperator.js
@@ -0,0 +1,12 @@
+const favMovie = "Star Wars: Episod IV";
+const timesSeen = 732;
+const goingToWatchItAgain = true; 
+
+// ADD CODE BELOW HERE
+const favMovieType = typeof favMovie
+const timesSeenType = typeof timesSeen
+const seeAgainType = typeof goingToWatchItAgain
+
+console.log(favMovieType)
+console.log(timesSeenType)
+console.log(seeAgainType)
