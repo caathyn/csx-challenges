f1dd14afe660acb2332b8ee485fed694017592a6
caathyn <caathynguyen@gmail.com>
2022-04-06 12:05:11 -0700

    solved forloopsfundamentals

---
+++ forLoopsFundamentals.js
@@ -0,0 +1,10 @@
+let countDown = 10;
+// // ADD CODE HERE
+for (let i = countDown; i > 0; i--) {
+	countDown--
+}
+  
+
+
+// Uncomment the below line to check your work
+console.log(countDown) // -> should print 0;
