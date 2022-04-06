778f6b3f2cd6f2d0a50f8ca66c60ca685c52c302
caathyn <caathynguyen@gmail.com>
2022-04-05 22:37:51 -0700

    solved booleans

---
+++ booleans.js
@@ -0,0 +1,13 @@
+let iHaveChanged = false;
+
+// ADD CODE BELOW
+iHaveChanged = true;
+const iLoveCoding = true;
+const codingIsTooHard = false;
+
+
+
+
+console.log(iHaveChanged)
+console.log(iLoveCoding)
+console.log(codingIsTooHard)
