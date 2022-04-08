0be2497e8fdcdc1e886a65217114732c58b788b1
caathyn <caathynguyen@gmail.com>
2022-04-07 18:00:59 -0700

    solved iteratingwithforloops

---
+++ iteratingWithForLoops.js
@@ -0,0 +1,16 @@
+const checkObj = {
+  oddNum: 1,
+  evenNum: 2,
+  foundNum: 5,
+  randomNum: 18
+};
+
+let divBy6 = false;
+// ADD CODE HERE
+const objToArray = Object.values(checkObj)
+// [1, 2, 5, 18]
+for (let i = 0; i < objToArray.length; i++) {
+  if (objToArray[i] % 6 === 0) {
+    divBy6 = true;
+  }
+}
