27b75054f7ffbb7c8d6e3a330aee5ab787df2929
caathyn <caathynguyen@gmail.com>
2022-04-08 12:42:57 -0700

    solve addtwo

---
+++ addTwo.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+function addTwo(num) { // num = 10
+  return num + 2
+}
+// Uncomment these to check your work!
+console.log(typeof addTwo); // should log: 'function'
+console.log(addTwo(10)); // should log: 12
