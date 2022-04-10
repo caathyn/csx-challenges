12d374b179c176b39cd640ff88cc9daf3469689c
caathyn <caathynguyen@gmail.com>
2022-04-10 10:34:31 -0700

    solve invokefunctions

--- invokeFunctions.js
+++ invokeFunctions.js
@@ -1,21 +1,23 @@
 let calls = "";
 
 function jerry(str) {
-
+  return kramer(str + 'Jerry');
 }
 
 function george(str) {
-
+  return elaine(str + 'George');
 }
 
 function elaine(str) {
-
+  return str + 'Elaine';
 }
 
 function kramer(str) {
-
+  return george(str + 'Kramer');
 }
 
 
 // Uncomment these to check your work!
-// calls = jerry(calls);
+calls = jerry(calls);
+
+console.log(calls);
