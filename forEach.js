45c5219a255969a48957ead0580ce5b77e0f9c90
caathyn <caathynguyen@gmail.com>
2022-04-16 20:08:06 -0700

    add foreach

---
+++ forEach.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+console.log(typeof forEach); // should log: 'function'
+forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
