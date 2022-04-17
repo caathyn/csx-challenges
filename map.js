ae950d277e4b61c972321d4bf39101c2605fc764
caathyn <caathynguyen@gmail.com>
2022-04-16 20:07:04 -0700

    add map

---
+++ map.js
@@ -0,0 +1,6 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+console.log(typeof subtractTwo); // should log: 'function'
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
