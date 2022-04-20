0c157481cfab61562c6fc07c21bbe1e88593ba28
caathyn <caathynguyen@gmail.com>
2022-04-20 10:29:55 -0700

    add multimap

---
+++ multiMap.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+function uppercaser(str) { return str.toUpperCase(); }
+function capitalize(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }
+function repeater(str) { return str + str; }
+const items = ['catfood', 'glue', 'beer'];
+const functions = [uppercaser, capitalize, repeater];
+console.log(multiMap(items, functions)); // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
