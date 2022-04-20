129656b0c588ec99d056b3c46e830187f58520cb
caathyn <caathynguyen@gmail.com>
2022-04-20 10:32:26 -0700

    add countby

---
+++ countBy.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+function evenOdd(n) {
+  if (n % 2 === 0) return 'even';
+  else return 'odd';
+}
+const nums = [1, 2, 3, 4, 5];
+console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
