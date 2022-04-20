893d234ff05a91c6290cde68dd88763aa971e4db
caathyn <caathynguyen@gmail.com>
2022-04-20 10:31:06 -0700

    add majority

---
+++ majority.js
@@ -0,0 +1,6 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+const isOdd = function(num) { return num % 2 === 1; };
+console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
