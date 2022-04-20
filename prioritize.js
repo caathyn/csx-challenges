b7b9ba67caaf880dd354bd52332b0f35312b2e12
caathyn <caathynguyen@gmail.com>
2022-04-20 10:31:53 -0700

    add prioritize

---
+++ prioritize.js
@@ -0,0 +1,6 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+function startsWithS(str) { return str[0].toLowerCase() === 's'; }
+const tvShows = ['curb', 'rickandmorty', 'seinfeld', 'sunny', 'friends']
+console.log(prioritize(tvShows, startsWithS)); // should log: ['seinfeld', 'sunny', 'curb', 'rickandmorty', 'friends']
