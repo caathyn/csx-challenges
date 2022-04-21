1525bc8c910e901e8b69aa0ce0956edbe85e7fce
caathyn <caathynguyen@gmail.com>
2022-04-21 00:49:26 -0700

    solved prioritize

--- prioritize.js
+++ prioritize.js
@@ -1,4 +1,17 @@
 // ADD CODE HERE
+const prioritize = (array, callback) => {
+  const trueArray = [];
+  const falseArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+   if (callback(array[i])) {
+     trueArray.push(array[i])
+   } else {
+     falseArray.push(array[i])
+   }
+  }
+  return trueArray.concat(falseArray);
+}
 
 // Uncomment these to check your work!
 function startsWithS(str) { return str[0].toLowerCase() === 's'; }
