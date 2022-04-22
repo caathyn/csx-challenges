5c873efc7d41b53010c75ce8edad28d906e9b4eb
caathyn <caathynguyen@gmail.com>
2022-04-21 20:04:31 -0700

    solved countBy

--- countBy.js
+++ countBy.js
@@ -1,4 +1,16 @@
 // ADD CODE HERE
+const countBy = (array, callback) => { 
+  const newObj = {};  
+  
+  for (let i = 0; i < array.length; i++) {
+    if (newObj[callback(array[i])] === undefined) {
+      newObj[callback(array[i])] = 0
+    }
+   	newObj[callback(array[i])]++
+  }
+  
+  return newObj;
+}
 
 // Uncomment these to check your work!
 function evenOdd(n) {
@@ -6,4 +18,4 @@ function evenOdd(n) {
   else return 'odd';
 }
 const nums = [1, 2, 3, 4, 5];
-console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+console.log(countBy(nums, evenOdd));
