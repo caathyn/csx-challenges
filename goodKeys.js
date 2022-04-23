1df842befc24f8a14046198c6504b9d9d62e67a3
caathyn <caathynguyen@gmail.com>
2022-04-22 22:52:50 -0700

    solved goodkeys

--- goodKeys.js
+++ goodKeys.js
@@ -1,5 +1,16 @@
 // ADD CODE HERE
 
+const goodKeys = (object, callback) => {
+  const newArray = [];
+  
+  for (let key in object) {
+   if (callback(object[key])) {
+     newArray.push(key)
+   }
+  }
+  return newArray;
+}
+
 // Uncomment these to check your work!
 const sunny = { mac: 'priest', dennis: 'calculator', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
 function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
