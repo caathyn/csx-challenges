3ffa171be17f3a55e9709d1e4b47fe0623ff697a
caathyn <caathynguyen@gmail.com>
2022-04-23 17:29:36 -0700

    add once

---
+++ once.js
@@ -0,0 +1,10 @@
+// ADD CODE HERE
+
+const addByTwoOnce = once(function(num) {
+  return num + 2;
+});
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+console.log(addByTwoOnce(5));  //should log 7
+console.log(addByTwoOnce(10));  //should log 7
+console.log(addByTwoOnce(9001));  //should log 7
