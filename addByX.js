07d9707bd59ced0c3a6f5f2d1c6df2bbc4c8728b
caathyn <caathynguyen@gmail.com>
2022-04-24 20:08:17 -0700

    solved addbyx

--- addByX.js
+++ addByX.js
@@ -1,8 +1,19 @@
 // ADD CODE HERE
+const addByX = (x) => {
+  const result = (num) => {
+    return num + x
+  }
+  return result;
+}
 
 const addByTwo = addByX(2);
+const addByThree = addByX(3);
 
 // Now call addByTwo with an input of 1 and log the output
-
+console.log(addByTwo(1));
 // Now call addByTwo with an input of 2 and log the output
+console.log(addByTwo(2));
 
+console.log(addByThree(1));
+console.log(addByThree(2));
+console.log(addByThree(3));
