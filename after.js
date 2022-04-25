72cb146d7c1ae9f59fb95c2742999566602dbe2c
caathyn <caathynguyen@gmail.com>
2022-04-24 20:28:40 -0700

    add after

---
+++ after.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+
+const called = function(string) { return('hello ' + string); };
+const afterCalled = after(3, called);
+
+// UNCOMMENT THESE LINES TO TEST YOUR WORK
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> 'hello world' is printed
