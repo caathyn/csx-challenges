46610546f8acb739e4592cfc54cdcb0696d72cb0
caathyn <caathynguyen@gmail.com>
2022-04-24 20:29:52 -0700

    add saveoutput

---
+++ saveOutput.js
@@ -0,0 +1,8 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // should log: 4
+console.log(multBy2AndLog(9)); // should log: 18
+console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
