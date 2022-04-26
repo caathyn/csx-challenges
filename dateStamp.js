9c346082cd82228a2a41ce743a5b7544ede00953
caathyn <caathynguyen@gmail.com>
2022-04-25 17:59:23 -0700

    add datestamp

---
+++ dateStamp.js
@@ -0,0 +1,6 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
