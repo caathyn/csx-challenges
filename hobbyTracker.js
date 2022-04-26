80616eac01d8958db6f16173b40d82970775c75d
caathyn <caathynguyen@gmail.com>
2022-04-26 16:57:00 -0700

    solved hobbytracker

--- hobbyTracker.js
+++ hobbyTracker.js
@@ -1,16 +1,27 @@
-function hobbyTracker(hobbies) {
+function hobbyTracker(hobbies) { // hobbies = ['yoga', 'baking', 'piano']
+  const cache = {} // { yoga: 0, baking: 0, piano: 0 }
+  const zeroOut = item => {
+    cache[item] = 0;
+  }
   
+  hobbies.forEach(zeroOut)
   
+  return (hobby, hours) => {
+    if (hobby === undefined) {
+    	hobbies.forEach(zeroOut)
+      
+      return 'tracker has been reset!'
+    }
+    cache[hobby] += hours
+    return cache
+  }
 }
-
-
-
 
 // Uncomment the code below to check your code:
 const updateHobbies = hobbyTracker(['yoga', 'baking', 'piano']);
-updateHobbies('yoga', 2);
-updateHobbies('baking', 4);
-updateHobbies('yoga', 1);
+updateHobbies('yoga', 2)
+updateHobbies('baking', 4)
+updateHobbies('yoga', 1)
 console.log(updateHobbies('piano', 2)); // --> { yoga: 3, baking: 4, piano: 2 }
 console.log(updateHobbies()); // --> 'tracker has been reset!'
 console.log(updateHobbies('baking', 1)); // --> { yoga: 0, baking: 1, piano: 0}
