d545086a735e4b8bef9bb738cecbcdb0a3f75297
caathyn <caathynguyen@gmail.com>
2022-04-25 18:00:24 -0700

    add censor

---
+++ censor.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+const changeScene = censor();
+changeScene('dogs', 'cats');
+changeScene('quick', 'slow');
+console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // should log: 'The slow, brown fox jumps over the lazy cats.'
