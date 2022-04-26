c95550c968e4a47a15d2d2e849acd855b1898c3e
caathyn <caathynguyen@gmail.com>
2022-04-26 16:57:35 -0700

    solved iloveevens

--- iLoveEvens.js
+++ iLoveEvens.js
@@ -9,4 +9,4 @@ function iLoveEvenNumbers(num) {
 
 // Uncomment these to check your work!
 console.log(iLoveEvenNumbers(42)); // expected log 'Oh Yeah Evens!'
-console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers'
+console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers' 
