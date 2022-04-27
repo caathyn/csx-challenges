5accf6234524812a1096a78bc2ccb1df14699bd9
caathyn <caathynguyen@gmail.com>
2022-04-27 09:58:19 -0700

    add factorial

---
+++ factorial.js
@@ -0,0 +1,7 @@
+function factorial(num) {
+
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+// console.log(factorial(4)); // -> 24
+// console.log(factorial(6)); // -> 720
