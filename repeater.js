5d96ad84d0085b80da2a85d920a226799d0f71bd
caathyn <caathynguyen@gmail.com>
2022-04-28 09:44:22 -0700

    solved repeater

--- repeater.js
+++ repeater.js
@@ -1,10 +1,13 @@
 console.log('Hello, world!');
 
 function repeater(char) {
-
+	if (char.length === 5) {
+      return char;
+      }
+  
+  return repeater(char + char[0]);
 }
 
 // To check if you've completed the challenge, uncomment these console.logs!
-// console.log(repeater('g'));
-// console.log(repeater('j'));
-
+console.log(repeater('g'));
+console.log(repeater('j'));
