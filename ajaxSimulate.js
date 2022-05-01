4856e1064dede8cd4a4656b4d556f8f7500302da
caathyn <caathynguyen@gmail.com>
2022-04-30 21:55:50 -0700

    solve ajaxsimulate

--- ajaxSimulate.js
+++ ajaxSimulate.js
@@ -3,6 +3,15 @@ let dataReceived;
 function ajaxSimulate(id, callback) {
   const database = ['Aaron', 'Barbara', 'Chris'];
   // Add code here
+  setTimeout(() => {
+    callback(database[id])
+  }, 0)
 }
 
 // Also add code here
+const storeData = data => {
+  dataReceived = data
+  console.log(dataReceived)
+}
+
+ajaxSimulate(1, storeData)
