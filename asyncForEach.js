0486518134263fa2045cdc268a71dbf609f9b247
caathyn <caathynguyen@gmail.com>
2022-05-01 21:31:16 -0700

    solved asyncforeach

--- asyncForEach.js
+++ asyncForEach.js
@@ -1,15 +1,15 @@
 const forEach = (arr, cb) => {
   for (let i = 0; i < arr.length; i++) {
-    cb(arr[i])
+    cb(arr[i], i);
   }
-}
+};
 
 const delays = [200, 500, 0, 350];
 
 const delayLog = (delayTime, i) => {
-  setTimeout(() => console.log(`printing element ${i}`), delayTime)
+  setTimeout(() => {
+    console.log(`printing element ${i}`)
+  }, delayTime)
 }
 
-forEach(delays, idx => {
-  delayLog(delays[idx], idx)
-})
+forEach(delays, delayLog)
