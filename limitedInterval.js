ee7efcf38b49a57cd2b24e60eb8e19ab0a4475e6
caathyn <caathynguyen@gmail.com>
2022-05-01 20:50:03 -0700

    add limitedinterval

---
+++ limitedInterval.js
@@ -0,0 +1,4 @@
+Add code here
+
+/* Uncomment the following line and click 'run' to test your work */
+limitedInterval(() => console.log('repeating'), 100, 550); // should log 'repeating' once per 100 ms, five times.
