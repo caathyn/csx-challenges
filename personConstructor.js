6f7e8cf75facb477ee97955b67148fd85808b739
caathyn <caathynguyen@gmail.com>
2022-05-03 10:23:31 -0700

    add personconstructor

---
+++ personConstructor.js
@@ -0,0 +1,10 @@
+function PersonConstructor() {
+	// add code here
+
+
+}
+
+const simon = new PersonConstructor();
+
+// Uncomment this line to check your work!
+// simon.greet(); // -> Logs 'hello'
