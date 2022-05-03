e4f55c9fcc2ede720252df63e3607b06564ab4b5
caathyn <caathynguyen@gmail.com>
2022-05-02 21:19:24 -0700

    add personfrompersonstore

---
+++ personFromPersonStore.js
@@ -0,0 +1,18 @@
+const personStore = {
+  greet: function() {
+    console.log('hello');
+  }
+}
+
+function personFromPersonStore(name, age) {
+	// add code here
+
+
+}
+
+const sandra = personFromPersonStore('Sandra', 26);
+
+// Uncomment these lines to check your work!
+// console.log(sandra.name); // -> Logs 'Sandra'
+// console.log(sandra.age); // -> Logs 26
+// sandra.greet(); // -> Logs 'hello'
