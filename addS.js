12305408af9c9ba7cb8061a56c0d11375b730303
caathyn <caathynguyen@gmail.com>
2022-06-29 11:45:26 -0700

    solved adds

--- addS.js
+++ addS.js
@@ -1,7 +1,5 @@
-// ADD CODE HERE
-function addS(str) {
-  return str + 's'
-}
+const addS = str => str + 's'
+
 // Uncomment these to check your work!
 console.log(typeof addS); // should log: 'function'
 console.log(addS('cat')); // should log: 'cats'
