761ba12c093c51129fc58b61ffb9c91de393e685
caathyn <caathynguyen@gmail.com>
2022-06-29 11:41:22 -0700

    solved addtwo

--- addTwo.js
+++ addTwo.js
@@ -1,7 +1,5 @@
-// ADD CODE HERE
-function addTwo(num) { // num = 10
-  return num + 2
-}
+const addTwo = num => num + 2
+
 // Uncomment these to check your work!
 console.log(typeof addTwo); // should log: 'function'
 console.log(addTwo(10)); // should log: 12
