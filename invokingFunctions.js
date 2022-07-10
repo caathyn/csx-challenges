f8dcc32dfb4fb317cea57e7d9a7f35b77f3fcb26
caathyn <caathynguyen@gmail.com>
2022-07-10 16:17:35 -0700

    solved invokingfunctions

---
+++ invokingFunctions.js
@@ -0,0 +1,24 @@
+let calls = "";
+
+function jerry(str) {
+  return kramer(str + "Jerry")
+
+}
+
+function george(str) {
+	return elaine (str + "George")
+}
+
+function elaine(str) {
+	return str + "Elaine"
+}
+
+function kramer(str) {
+	return george(str + "Kramer")
+}
+
+
+// Uncomment these to check your work!
+calls = jerry(calls);
+console.log(calls); // should log: 'JerryKramerGeorgeElaine'
+
