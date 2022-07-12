785b48a0d5a378c81b2fa661f207c998477020aa
caathyn <caathynguyen@gmail.com>
2022-07-12 13:09:43 -0700

    solved pluralize

--- pluralize.js
+++ pluralize.js
@@ -1,4 +1,12 @@
-
+const pluralize = string => {
+  let newArray = [];
+  for (let i = 0; i < string.length; i++) {
+    let newStr = string[i] + "s";
+    newArray.push(newStr);
+  }
+  
+  return newArray;
+}
 
 // Uncomment these to check your work!
 const animals = ["dog", "cat", "tree frog"];
