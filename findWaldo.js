6722f74335b7fdcff7a2c573254ae84b1b4a3e3b
caathyn <caathynguyen@gmail.com>
2022-07-12 12:53:41 -0700

    solved findwaldo

---
+++ findWaldo.js
@@ -0,0 +1,8 @@
+const addWaldo = obj => {
+  const name = "Waldo";
+  obj[name] = "unknown";
+  return obj;
+}
+// Uncomment these to check your work!
+const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
