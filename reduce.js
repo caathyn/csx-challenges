ed16b347da919c206d39ff36a1562d46f4bc621e
caathyn <caathynguyen@gmail.com>
2022-07-13 12:35:28 -0700

    solved reduce

---
+++ reduce.js
@@ -0,0 +1,13 @@
+const reduce = (array, callback, initialValue) => {
+  let accumulator = initialValue;
+  for (let i = 0; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i]);
+  }
+  
+  return accumulator;
+}
+
+// Uncomment these to check your work!
+const nums = [4, 1, 3];
+const add = function(a, b) { return a + b; }
+console.log(reduce(nums, add, 0)); // should log 8
