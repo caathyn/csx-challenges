f2c5241ad13a9bf53f23ad695833687330b6d73c
caathyn <caathynguyen@gmail.com>
2022-07-17 14:21:36 -0700

    solved majority

--- majority.js
+++ majority.js
@@ -1,4 +1,20 @@
-// ADD CODE HERE
+function majority(array, callback) {
+  let trueCount = 0;
+  let falseCount = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      trueCount++;
+    } else {
+    	falseCount++;
+    }
+  }
+  
+  if (trueCount > falseCount) {
+    return true;
+  }
+  
+  return false;
+}
 
 // Uncomment these to check your work!
 const isOdd = function(num) { return num % 2 === 1; };
