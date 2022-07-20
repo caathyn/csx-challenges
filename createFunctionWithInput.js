6678a7c33e929b4e42268c92b08b799b34b7f640
caathyn <caathynguyen@gmail.com>
2022-07-19 20:58:15 -0700

    add createfunctionwithinput

---
+++ createFunctionWithInput.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const sampleFunc = createFunctionWithInput('sample');
+console.log(sampleFunc()); // should log: 'sample'
+const helloFunc = createFunctionWithInput('hello');
+console.log(helloFunc()); // should log: 'hello'
