ac4e86181b08ff5a5043b7be8dcdf807f8d95339
caathyn <caathynguyen@gmail.com>
2022-07-19 21:01:15 -0700

    solved countby

--- countBy.js
+++ countBy.js
@@ -1,20 +1,39 @@
 const countBy = (array, callback) => {
-  const obj = {};
-  let count = 0;
-  for (let i = 1; i < array.length; i++) {
-    if (callback(array[i])) {
-      obj[callback(array[i])] = count++
-      
+  const obj = {}; 
+  
+  for (let i = 0; i < array.length; i++) {
+    const key = callback(array[i])
+    if (!obj[key]) {
+    	obj[key] = 0;
     }
+    obj[key]++;
   }
   
   return obj;
 }
 
 // Uncomment these to check your work!
-function evenOdd(n) {
-  if (n % 2 === 0) return 'even';
+function evenOddZero(n) {
+  if (n === 0) return 'zero';
+  else if (n % 2 === 0) return 'even'
   else return 'odd';
 }
-const nums = [1, 2, 3, 4, 5];
-console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+const nums = [5, 0, 2, 0, 3, 4, 1]; // [ zero, odd, even, zero, odd, even, odd]
+console.log(countBy(nums, evenOddZero)); // should log: { zero: 2, odd: 3, even: 2 }
+
+function pokedex(pokemon) {
+	const types = {
+  	pikachu: 'electric',
+    electabuzz: 'electric',
+    charizard: 'fire',
+    squirtle: 'water',
+    abra: 'psychic',
+    growlithe: 'fire'
+  }
+  
+  return types[pokemon];
+}
+
+const pokemon = ['pikachu', 'charizard', 'squirtle', 'electabuzz', 'abra', 'growlithe'];
+
+console.log(countBy(pokemon, pokedex)); // should log: { odd: 3, even: 2 }
