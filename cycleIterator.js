592b9215281bf4ebcaf2af0388f751c817b91d55
caathyn <caathynguyen@gmail.com>
2022-07-22 18:00:22 -0700

    solved cycleiterator

---
+++ cycleIterator.js
@@ -0,0 +1,22 @@
+const cycleIterator = array => {
+  let index = 0;
+  const week = () => {
+    let i = index++;
+    if (index >= array.length) { 
+      index = 0;
+    }
+    
+    return array[i]
+  }
+  
+  return week;
+}
+
+// Uncomment these to check your work!
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // should log: 'Fri'
+console.log(getDay()); // should log: 'Sat'
+console.log(getDay()); // should log: 'Sun'
+console.log(getDay()); // should log: 'Fri'
+console.log(getDay()); // should log: 'Fri'
