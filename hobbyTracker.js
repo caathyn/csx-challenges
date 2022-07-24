6954b415e5d6614a4c42d15c97351922c55be141
caathyn <caathynguyen@gmail.com>
2022-07-23 21:52:45 -0700

    add hobbytracker

---
+++ hobbyTracker.js
@@ -0,0 +1,16 @@
+function hobbyTracker(hobbies) {
+  
+  
+}
+
+
+
+
+// Uncomment the code below to check your code:
+const updateHobbies = hobbyTracker(['yoga', 'baking', 'piano']);
+updateHobbies('yoga', 2);
+updateHobbies('baking', 4);
+updateHobbies('yoga', 1);
+console.log(updateHobbies('piano', 2)); // --> { yoga: 3, baking: 4, piano: 2 }
+console.log(updateHobbies()); // --> 'tracker has been reset!'
+console.log(updateHobbies('baking', 1)); // --> { yoga: 0, baking: 1, piano: 0}
