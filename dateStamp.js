2905084577ab9ecaa6ca43794e7f88286e02264a
caathyn <caathynguyen@gmail.com>
2022-07-24 16:07:07 -0700

    solved datestamp

---
+++ dateStamp.js
@@ -0,0 +1,24 @@
+function dateStamp(func) {
+  return function(...arg) {
+    const obj = {};
+    obj.date = new Date().toDateString();
+    obj.output = func(...arg)
+    return obj;
+  }
+}
+// Uncomment these to check your work!
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
+function dateStamp(func) {
+  return function(...arg) {
+    const obj = {};
+    obj.date = new Date().toDateString();
+    obj.output = func(...arg)
+    return obj;
+  }
+}
+// Uncomment these to check your work!
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
