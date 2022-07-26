51d3c14fba068bae4ca6e710b040839bd4945040
caathyn <caathynguyen@gmail.com>
2022-07-26 14:24:35 -0700

    solved makeperson

---
+++ makePerson.js
@@ -0,0 +1,10 @@
+function makePerson(name, age) {
+	return {name, age}
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
