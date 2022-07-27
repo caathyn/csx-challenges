1a77e16d81a555b864d4e0dcc50367704de7a021
caathyn <caathynguyen@gmail.com>
2022-07-27 13:15:44 -0700

    solved personclass

---
+++ PersonClass.js
@@ -0,0 +1,17 @@
+class PersonClass {
+	constructor(name) {
+    this.name = name
+
+	}
+
+	greet() {
+    console.log('hello')
+  }
+
+}
+
+const george = new PersonClass('George');
+
+// Uncomment this line to check your work!
+george.greet(); // -> Logs 'hello'
+
