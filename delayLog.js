b6c96e41d5f49c37cdb73bbbe591a16527cf67c4
caathyn <caathynguyen@gmail.com>
2022-07-28 10:01:56 -0700

    solved delaylog

---
+++ delayLog.js
@@ -0,0 +1,13 @@
+function forEach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i)
+  }
+}
+
+const delays = [200, 500, 0, 350]
+
+function delayLog(delayTime, i) {
+  setTimeout(() => console.log(`printing element ${i}`), delayTime)
+}
+
+forEach(delays, delayLog)
