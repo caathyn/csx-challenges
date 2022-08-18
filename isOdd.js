583d8a2820d1cfba555426b23f434686af086ba5
caathyn <caathynguyen@gmail.com>
2022-08-18 16:52:35 -0700

    solved isodd

---
+++ isOdd.js
@@ -0,0 +1,11 @@
+function isOdd(number) {
+  if (number % 2 !== 0) {
+    return true;
+  }
+  
+  return false;
+}
+
+// Uncomment these to check your work!
+console.log(isOdd(5)); // should log: true
+console.log(isOdd(2008)); // should log: false
