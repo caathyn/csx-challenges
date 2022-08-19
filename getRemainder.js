03089b482dd19290dc395c7c8a27265f64a2ffb9
caathyn <caathynguyen@gmail.com>
2022-08-19 12:16:32 -0700

    solved getremainder

--- getRemainder.js
+++ getRemainder.js
@@ -1,9 +1,9 @@
 function getRemainder(num1, num2) {
   if (num1 > num2) {
-    return num1 % num2
-  } else {
-    return num2 % num1;
+    return num1 % num2;
   }
+
+  return num2 % num1;
 }
 
 // Uncomment the lines below to test your code
