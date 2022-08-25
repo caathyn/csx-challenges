34cecb5b88e055927b03e4d74bd5b0b7376ccedc
caathyn <caathynguyen@gmail.com>
2022-08-25 15:50:17 -0700

    solved closesttothemark

--- closestToTheMark.js
+++ closestToTheMark.js
@@ -1,14 +1,16 @@
 function closestToTheMark(player1, player2){
   const theMark = Math.floor(Math.random() * 100)
   console.log(`If theMark is ${theMark}...`);
- 	let player1Distance = Math.abs(player1 - theMark)
-  let player2Distance = Math.abs(player2 - theMark)
-  if (player1Distance < player2Distance) {
-    return "Player 1 is the closest"
-  } else if (player1Distance > player2Distance) {
-    return "Player 2 is the closest"
+  
+  const player1Distance = Math.abs(theMark - player1)
+  const player2Distance = Math.abs(theMark - player2)
+  
+  if (player1Distance > player2Distance) {
+    return "Player 1 is closest"
+  } else if (player2Distance > player1Distance) {
+    return "Player 2 is closest"
   } else {
-    return "It's a tie."
+    return "It's a tie"
   }
 }
 
