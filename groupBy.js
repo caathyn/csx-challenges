2864381980b0fa1740dc1b93c5460cf57b92ea45
caathyn <caathynguyen@gmail.com>
2022-08-29 13:12:21 -0700

    solved groupby

--- groupBy.js
+++ groupBy.js
@@ -1,13 +1,15 @@
-const groupBy = (array, callback) => {
+function groupBy(array, callback) {
   const obj = {};
-  for (let i = 0; i < array.length; i++) {
-    if (!obj[callback(array[i])]) {
-      obj[callback(array[i])] = [array[i]]
+  
+  for (let i = 0; i < array.length; i++) { 
+    const key = callback(array[i])
+     if (!obj[key]) {
+      obj[key] = [array[i]]
     } else {
-      obj[callback(array[i])].push(array[i])
+      obj[key].push(array[i])
     }
   }
-  
+    
   return obj;
 }
 
@@ -15,4 +17,3 @@ const groupBy = (array, callback) => {
 const decimals = [1.3, 2.1, 2.4];
 const floored = function(num) { return Math.floor(num); };
 console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
-
