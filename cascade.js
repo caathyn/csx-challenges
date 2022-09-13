a6499194667eaecb8e510b581e5a63dfd9698f15
caathyn <caathynguyen@gmail.com>
2022-09-13 14:09:29 -0700

    solved cascade

--- cascade.js
+++ cascade.js
@@ -1,16 +1,15 @@
 function cascade(number) {
-	// Your code here!
+  if (typeof number !== "number") return; 
+  
+	let strNum = number.toString();
+  console.log(+strNum)
+  if (+strNum.length === 1) return;
+  
+  cascade(+strNum.slice(0, -1))
+  console.log(+strNum)
 }
 
 
 
-// // Uncomment to test your work!
-// cascade(111)
-// // should print
-// /*
-// 111
-// 11
-// 1
-// 11
-// 111
-// */
+// Uncomment to test your work!
+console.log(cascade(1234))
