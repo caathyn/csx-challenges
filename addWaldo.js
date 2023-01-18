305ca1ee125feea8db2d2ce9866e4065f46846f6
caathyn <caathynguyen@gmail.com>
2023-01-18 11:12:51 -0800

    testing

--- addWaldo.js
+++ addWaldo.js
@@ -6,3 +6,4 @@ const addWaldo = obj => {
 // Uncomment these to check your work!
 const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
 console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
+console.log('hello')
